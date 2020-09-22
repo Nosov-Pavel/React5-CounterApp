376f3d4b59bd1ce70fa68dbe9cd581f2980de623
Nosov-Pavel <nopaev@gmail.com>
2020-09-21 23:04:28 -0400

    new component added App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import ListItem from "./listItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+     const [list, setList] = useState([1, 2, 3, 4]);
+     const counterPlus = (index) => {
+         console.log('index', index)
+        const newArr = list.map((el, i) =>{
+            if (index === i) {
+                return el + 1
+            }
+
+            return el
+        })
+         console.log('newArr', newArr)
+
+         setList(newArr)
+
+    }
+
+    return (
+        <div>
+            {list.map( (el, index) => {
+               return <ListItem counterPlus={counterPlus} index={index} el={el} key={Math.random()}/>
+            })}
+
+        </div>
+    );
 }
 
 export default App;
