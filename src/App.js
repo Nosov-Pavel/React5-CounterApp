93b7069aa206d57a01b4381fd2552be1983d4be3
Nosov-Pavel <nopaev@gmail.com>
2020-12-08 12:34:21 -0500

    strart

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
-import React from "react";
-import Menu from "./Menu";
-import Menu2 from "./Menu2";
-import Menu3 from "./Menu3";
-import Menu4 from "./Menu4";
+import React, {useState} from "react";
+import './App.css';
 
-const menu = [
-    { title: 'Home', link: '/'},
-    { title: 'Products', link: '/products'},
-    { title: 'Contacts', link: '/contacts'},
-  ];
-
-const menu2 = [
-    {title2: 'Address', link: '/address' },
-    {title2: 'City', link: '/city' },
-    {title2: 'Country', link: '/country' },
-];
-
-const person = [
-    {title3: 'Name', link: '/'},
-    {title3: 'Midle Name', link: '/midleName'},
-    {title3: 'Firs Name', link: '/firstName'},
-];
-
-const status = [
-    {title4: 'Correct', link: '/'},
-    {title4: 'Un Correct', link: '/uncorrect'},
-    {title4: 'Defolt', link: '/defolt'},
-];
+function App() {
 
 
-export default function App(){
+}
+
+
+
     return(
         <div>
-            <h1>Hello WebS</h1>
-            <Menu menu={menu}/>
-            <Menu2 menu2={menu2}/>
-            <Menu3 person={person}/>
-            <Menu4 status={status}/>
+
         </div>
     );
 }
+export default  App;
--- src/Menu4.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-export default function Menu4(props){
-    const j = props.status.map(el => <li key={el.title4}>{el.title4}</li>)
-    return(
-        <ul>{j}</ul>
-    )
-}
