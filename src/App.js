35b5a9a4e1f4935373629b307d9ae74992c8229d
Nosov-Pavel <nopaev@gmail.com>
2020-12-08 12:44:38 -0500

    user2

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, {useState} from "react";
 import './App.css';
 
 function App() {
-const intitItem = {id:1, user:'Pavel', gender:'m', active: true};
+    const intitItem = {id: 1, user: 'Pavel', gender: 'm', active: true};
 
+    const intinItem = {id: 2, user: 'Petya', gender: 'm', active: false};
 
+    const [list, setList1] = useState([intinItem1]);
+    const [list, setList1] = useState([intinItem2]);
+
+return (
+    <div className='App'>
+
+    </div>
+);
 }
-
-
-
-    return(
-        <div>
-
-        </div>
-    );
-}
-export default  App;
+export default App;
