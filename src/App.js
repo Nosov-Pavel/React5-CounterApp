ae44323db3d7c119aaa8079add98a88f0a85722b
Nosov-Pavel <nopaev@gmail.com>
2020-12-09 13:00:30 -0500

    buttonMove

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ return (
     <div className='App'>
         TODO
         {list1.map(el=> <li>{el.user}**{el.gender}**{el.active.toString()}
-        </li>)}
+        <button>Move to other List</button>
+        </li>)
+        }
 
     </div>
 );
